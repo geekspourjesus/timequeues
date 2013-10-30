77e6aea25bc29f8db2f15eaedb5f4a996215ad7e
geekspourjesus <geekspourjesus@gmail.com>
2013-10-30 05:32:59 +0100

    Update install.php

--- install.php
+++ install.php
@@ -28,6 +28,10 @@ else  {
 		`deptname` VARCHAR( 50 )
 	)
 	";
+$sql = " ALTER TABLE `timeconditions` ADD `timequeue` VARCHAR(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NULL DEFAULT NULL AFTER `falsegoto`,
+ADD`agent4` VARCHAR(50) ,
+ADD `agent5` VARCHAR(50) ,
+ADD `agent6` VARCHAR(50) AFTER `timequeue`";
  
 }
 $check = $db->query($sql);
